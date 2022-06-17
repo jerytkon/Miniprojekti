659e29b31e33542f149e4cea5c27b6d5a0704dfc
Jere Rytkönen <jere.rytkonen@awacademy.training>
2022-06-17 11:09:08 +0200

    propsit ja constructori tehty

--- Tiedostokuvaus/Class1.cs
+++ Tiedostokuvaus/Class1.cs
@@ -2,11 +2,34 @@
 {
     public class Class1
     {
-        prop jsjfhsdjaöjfs'
+        public int CourseCode { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Location { get; set; }
+        public bool MaterialType { get; set; }
+        public string Description { get; set; }
+        public int MatCode { get; set; }
+        public string MainCategory { get; set; }
+        public string SubCategory1 { get; set; }
+        public string SubCategory2 { get; set; }
+        public Class1(int courseCode, string name, DateTime startDate, DateTime endDate, string location, bool materialType, string description, int matCode, string mainCategory, string subCategory1, string subCategory2)
+        {
+            CourseCode = courseCode;
+            Name = name;
+            StartDate = startDate;
+            EndDate = endDate;
+            Location = location;
+            MaterialType = materialType;
+            Description = description;
+            MatCode = matCode;
+            MainCategory = mainCategory;
+            SubCategory1 = subCategory1;
+            SubCategory2 = subCategory2;
+        }
 
-            jaefjjljsfsf
-            jhkhahsdhdahad
 
-            jklfjkfjdsfsd
+
+
     }
 }
